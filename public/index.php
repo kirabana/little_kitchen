87c4448536abfc85d99ea5466945fc937f799d07
Hikari Tsukui <evenex@hotmail.com>
2014-07-02 18:21:38 +1200

    Started. Added in style

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Little Kitchen</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div id="header">
+		<div class="logo"></div>
+		<nav>
+			<ul>
+			
+				<li><a href="">BLOG</a></li>
+				<li><a href="">ABOUT</a></li>
+				<li><a href="">CONTACT</a></li>
+				<li><a href="">LOGIN</a></li>
+				<li><a href="">REGISTER</a></li>		
+			</ul>
+		</nav>
+	</div>
+</body>
+</html>
