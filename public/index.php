a4da913d116ed9e951d069e8339547600c83c054
Hikari Tsukui <evenex@hotmail.com>
2014-07-03 17:59:55 +1200

    More styles added

--- public/index.php
+++ public/index.php
@@ -3,21 +3,51 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Little Kitchen</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans|Homemade+Apple' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	<div id="header">
 		<div class="logo"></div>
 		<nav>
-			<ul>
-			
-				<li><a href="">BLOG</a></li>
-				<li><a href="">ABOUT</a></li>
-				<li><a href="">CONTACT</a></li>
-				<li><a href="">LOGIN</a></li>
-				<li><a href="">REGISTER</a></li>		
+			<ul class=left>
+				<li class="borderRight"><a href="">BLOG</a></li>
+				<li class="borderRight"><a href="">ABOUT</a></li>
+				<li><a href="">CONTACT</a></li>		
+			</ul>
+
+			<ul class=right>
+				<li class="borderRight"><a href="">LOGIN</a></li>
+				<li><a href="">REGISTER</a></li>	
 			</ul>
 		</nav>
 	</div>
+
+	<div class="container">
+		<div class="col-9">
+			<div class="col-5">
+				<img src="assets/img/fried_rice.png" alt="">
+			</div>
+			<div class="col-6">
+				<h2>Title Comes Here</h2>
+				<p>Cras nec mauris eget enim volutpat ornare. Donec eget lorem ac metus porta sodales. Nam ut lectus ut metus mattis vehicula. Nunc nec massa mauris. </p>
+			</div>
+		</div>
+
+		<div class="col-2">
+			<h2>Category</h2>
+			<h3>Asian</h3>
+			<h3>Baking</h3>
+			<h3>Breakfast</h3>
+			<h3>Dinner</h3>
+			<h3>Fish</h3>
+			<h3>Japanese</h3>
+			<h3>Lunch</h3>
+			<h3>Meat</h3>
+			<h3>Vegetable</h3>
+			<h3>Western</h3>
+		</div>
+	</div>
+
 </body>
 </html>
